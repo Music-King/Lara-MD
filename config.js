3c6d949cca885d0a366b3616a80e67c739fec314
Music-King <Mk4744639@gmail.com>
2024-12-21 22:57:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "SzZ10ZrK#s8UeEavCyZi0QW_vcC94dEXjHjrU-B_4Xg8_lqV0bnA", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
